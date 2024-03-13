686775a028ec63be6b3621c9ab46df44c4581d4b
birdgang <birdgang82@gmail.com>
2024-03-13 15:14:39 +0900

    init smart-contract

---
+++ smart-contract/README.md
@@ -0,0 +1,65 @@
+# setup
+````
+npm init
+npm install hardhat
+
+````
+
+
+# init
+````
+npx hardhat
+
+````
+
+
+# build & deploy & verify
+````
+
+> npx hardhat compile
+> npx hardhat run scripts/deploy-erc20-token.js --network sepolia
+> npx hardhat verify --network sepolia 0x5865b234E2D690c5A63CE5B843e6A27DC3365583 --contract contracts/TOKEN/BIRDGANG.sol:BIRDGANG
+
+````
+
+
+# test
+
+````
+
+````
+
+
+# token
+```` 
+
+* token address : 0x5865b234E2D690c5A63CE5B843e6A27DC3365583
+* scan ref : https://sepolia.etherscan.io/tx/0x4def11bb07cf3ef5363fedbe6c3a2f765f742430bee3464a8c9a72951907720f
+* verify ref : https://sepolia.etherscan.io/address/0x5865b234E2D690c5A63CE5B843e6A27DC3365583#code
+
+````
+
+
+
+# link
+
+````
+https://docs.openzeppelin.com/
+
+get chain key support
+https://app.infura.io/
+
+get chain key
+https://sepolia.etherscan.io/
+
+https://hardhat.org/
+
+````
+
+
+
+# metadata
+````
+ETH chain - sepolia
+````
+
---
+++ smart-contract/hardhat.config.js
@@ -0,0 +1,55 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-ethers");
+
+const {
+  INFURA_KEY,
+  DEPLOYER_PRIVATE_KEY,
+  ETHERSCAN_KEY,
+  POLYGON_KEY_MUMBAI
+} = process.env
+
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "sepolia",
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
+    },
+    // gpextest: {
+    //   url: `https://testnet.rpc.gb.gpexdev.com`,
+    //   accounts: [`${DEPLOYER_PRIVATE_KEY}`]
+    // },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: ETHERSCAN_KEY,
+      sepolia: ETHERSCAN_KEY,
+      // polygon
+      polygon: POLYGON_KEY_MUMBAI,
+      // mumbai: POLYGON_KEY_MUMBAI,
+      polygonMumbai: POLYGON_KEY_MUMBAI,
+    }
+  }
+};
